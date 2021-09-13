05a38a7b8ad9cc7a8f829f4378897ece94349eaa
Anthony <anthony.gilbert93@gmail.com>
2021-09-13 18:43:08 -0500

    71. Composition API Basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Component </h1>
+    <h3>hello, my name is {{name}} and my age is {{age}}</h3>
   </div>
+  <button @click="handleClick">Click Me</button>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+
+  const handleClick = () => {
+    console.log('you clicked me');
+  }
+
+    return {
+      name,
+      age,
+      handleClick
+    }
   }
 }
 </script>
