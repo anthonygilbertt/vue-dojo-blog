58691ba9ae46bc83c62f095e49232f84b72a426e
Anthony <anthony.gilbert93@gmail.com>
2021-09-13 19:57:00 -0500

    72. Template Refs in the Setup() function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <h1>Home Component </h1>
-    <h3>hello, my name is {{name}} and my age is {{age}}</h3>
+    <h3 ref="h3">hello, my name is {{name}} and my age is {{age}}</h3>
   </div>
   <button @click="handleClick">Click Me</button>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 // import HelloWorld from '@/components/HelloWorld.vue'
@@ -17,18 +18,27 @@ export default {
     // HelloWorld
   },
   setup() {
+    console.log(this);
+
+    const h3 = ref(null)
+    console.log(h3, h3.value);
+
     let name = 'mario'
     let age = 30
 
 
   const handleClick = () => {
     console.log('you clicked me');
+    console.log(h3, h3.value);
+    h3.value.classList.add('test')
+    h3.value.textContent = 'hello'
   }
 
     return {
       name,
       age,
-      handleClick
+      handleClick,
+      h3
     }
   }
 }
