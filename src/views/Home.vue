41d3545dfd65a15d23782cd7ded671afe0c45507
Anthony <anthony.gilbert93@gmail.com>
2021-09-13 20:05:25 -0500

    73. Using Refs for Reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
     <h3 ref="h3">hello, my name is {{name}} and my age is {{age}}</h3>
   </div>
   <button @click="handleClick">Click Me</button>
+  <button @click="age++">Add 1 to age</button>
+  <input type="text" v-model="name">
 </template>
 
 <script>
@@ -23,22 +25,22 @@ export default {
     const h3 = ref(null)
     console.log(h3, h3.value);
 
-    let name = 'mario'
-    let age = 30
+    let name = ref('mario')
+    let age = ref(30)
 
+    // name.value
+    // age.value
 
   const handleClick = () => {
-    console.log('you clicked me');
-    console.log(h3, h3.value);
-    h3.value.classList.add('test')
-    h3.value.textContent = 'hello'
+    name.value = 'luigi'
+    age.value = 35
+
   }
 
     return {
       name,
       age,
-      handleClick,
-      h3
+      handleClick
     }
   }
 }
