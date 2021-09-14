76656f80ebf723e8836e3af1ee23478163cfecb6
Anthony <anthony.gilbert93@gmail.com>
2021-09-14 16:59:32 -0500

    77. Using Props in the Setup() function

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-list">
+  <h1>Post List Component</h1>
+  <!--Cycle through some posts here-->
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post" />
+  </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from '@/components/SinglePost.vue'
+
+export default {
+    name: 'PostList',
+    components: { SinglePost },
+    props: ['posts'],
+    setup(props) {
+        console.log(props.posts);
+
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,39 @@
 <template>
   <div class="home">
     <h1>Home Component </h1>
+    <PostList :posts="posts"/>
+
+    
+<!--
     <input type="text" v-model="search">
     <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-
     <button @click="handleClick">Stop Watching</button>
+-->
 
   </div>
 </template>
 
 <script>
 import { computed, ref } from '@vue/reactivity'
-import { watch, watchEffect } from '@vue/runtime-core'
+// import { watch, watchEffect } from '@vue/runtime-core'
+import PostList from '@/components/PostList.vue'
 // @ is an alias to /src
 
 
 export default {
   name: 'Home',
-  
+  components: { 
+    PostList
+  },
   setup() {
-    const search = ref('')
-    const names = ref([
-      'mario',
-      'yoshi',
-      'luigi',
-      'toad',
-      'bowser',
-      'koopa',
-      'peach'
+    
+    
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'some text some text some text some text some text', id: 1},
+      {title: 'How to dockerize your vue', body: 'some text', id: 2}
     ])
 
-
-    const stopWatch = watch(search, () => {
-      console.log('watch functin run');
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function run');
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    }
-
-    
-
-    return { names, search, matchingNames, handleClick}
-
+    return { posts }
   }
 }
 </script>
