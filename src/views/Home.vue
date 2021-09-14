c96d0cb81a819fb627aef9fe906d67f8a209351e
Anthony <anthony.gilbert93@gmail.com>
2021-09-13 21:14:26 -0500

    75. Computed Values in the Setup() Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="home">
     <h1>Home Component </h1>
-    <h2>Refs</h2>
-    <p>{{ novaOne.name }} - {{ novaOne.age }}</p>
-    <button @click="updateNovaOne">Update Nova One</button>
-
-    <h2>Reactive</h2>
-    <p>{{ novaTwo.name }} - {{ novaTwo.age }}</p>
-    <button @click="updateNovaTwo">Update Nova Two</button>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
 
   </div>
 </template>
 
 <script>
-import { ref, reactive } from '@vue/reactivity'
+import { computed, ref } from '@vue/reactivity'
 // @ is an alias to /src
 
 
@@ -21,19 +17,24 @@ export default {
   name: 'Home',
   
   setup() {
-    const novaOne = ref({ name: 'mario', age: 30 })
-    const novaTwo = reactive({ name: 'luigi', age: 35 })
+    const search = ref('')
+    const names = ref([
+      'mario',
+      'yoshi',
+      'luigi',
+      'toad',
+      'bowser',
+      'koopa',
+      'peach'
+    ]);
 
-    const updateNovaOne = () => {
-      novaOne.value.name = 'nova1'
-      novaOne.value.age = 28
-    }
-    const updateNovaTwo = () => {
-      novaTwo.name = 'nova2'
-      novaTwo.age = 29
-    }
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
 
-    return { novaOne, updateNovaOne ,novaTwo , updateNovaTwo}
+    
+
+    return { names, search, matchingNames }
 
   }
 }
